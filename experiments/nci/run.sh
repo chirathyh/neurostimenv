f8de6931646de0ea510f81235a1dd8617c5cc684
chirathyh <chirathyh@gmail.com>
2024-10-28 15:00:06 +1100

    nci job script

---
+++ experiments/nci/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P ny83
+#PBS -q hugemem
+#PBS -l walltime=15:50:00
+#PBS -l mem=380GB
+#PBS -l jobfs=100GB
+#PBS -l ncpus=96
+#PBS -M chirath.hettiarachchi@anu.edu.au
+#PBS -l storage=gdata/ny83
+#PBS -o out.txt
+#PBS -e err.txt
+#PBS -l software=python
+
+module load python3/3.10.4
+module load openmpi/4.1.1
+source /g/data/ny83/ch9972/NeuroStim/bin/activate
+
+mpirun -np 72 python3 /g/data/ny83/ch9972/NeuroStim/neurostimenv/experiments/hydra_test.py experiment.name=test33
+wait
