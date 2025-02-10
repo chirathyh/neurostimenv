7dce25c2d1f2a07802fe30f378bb6ae0b03d86a0
chirathyh <chirathyh@gmail.com>
2025-02-11 10:13:46 +1100

    refactor

--- experiments/nci/run_mdd.sh
+++ experiments/nci/run_mdd.sh
@@ -14,10 +14,8 @@
 module load python3/3.10.4
 module load openmpi/4.1.1
 source /g/data/ny83/ch9972/NeuroStim/bin/activate
-cd /g/data/ny83/ch9972/NeuroStim/neurostimenv/setup/circuits/L23Net/mod
-nrnivmodl
 cd /g/data/ny83/ch9972/NeuroStim/neurostimenv
 
-mpirun -np 512 python3 experiments/feature_analysis/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=1000 env.network.dt=0.025 env.simulation.MDD=True env.ts.apply=False env.network.syn_activity=True experiment.debug=False
+mpirun -np 32 python3 experiments/feature_analysis/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=100 env.network.dt=0.025 env.simulation.MDD=True env.ts.apply=False env.network.syn_activity=True experiment.debug=False
 
 wait
