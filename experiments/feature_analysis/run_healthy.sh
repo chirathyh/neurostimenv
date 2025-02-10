e3a523e90afe04cc374b9e8db23a8bd6efe1f383
chirathyh <chirathyh@gmail.com>
2025-02-11 09:53:31 +1100

    nci exp

--- experiments/feature_analysis/run_healthy.sh
+++ experiments/feature_analysis/run_healthy.sh
@@ -16,6 +16,6 @@ module load openmpi/4.1.1
 source /g/data/ny83/ch9972/NeuroStim/bin/activate
 cd.. /g/data/ny83/ch9972/NeuroStim/neurostimenv
 
-mpirun -np 16 python3 experiments/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=1000 env.network.dt=0.025 env.simulation.MDD=False env.ts.apply=False env.network.syn_activity=True experiment.debug=False
+mpirun -np 16 python3 experiments/feature_analysis/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=1000 env.network.dt=0.025 env.simulation.MDD=False env.ts.apply=False env.network.syn_activity=True experiment.debug=False
 
 wait
--- experiments/feature_analysis/run_mdd.sh
+++ experiments/feature_analysis/run_mdd.sh
@@ -18,6 +18,6 @@ cd /g/data/ny83/ch9972/NeuroStim/neurostimenv/setup/circuits/L23Net/mod
 nrnivmodl
 cd /g/data/ny83/ch9972/NeuroStim/neurostimenv
 
-mpirun -np 512 python3 experiments/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=1000 env.network.dt=0.025 env.simulation.MDD=True env.ts.apply=False env.network.syn_activity=True experiment.debug=False
+mpirun -np 512 python3 experiments/feature_analysis/run_simulations.py experiment.name=test env=hl23net env.simulation.duration=1000 env.network.dt=0.025 env.simulation.MDD=True env.ts.apply=False env.network.syn_activity=True experiment.debug=False
 
 wait
