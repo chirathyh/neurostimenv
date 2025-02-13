19148a3826afb29990e7a3175e04ddec529f2143
chirathyh <chirathyh@gmail.com>
2025-02-13 15:20:06 +1100

    exp params nci

--- experiments/nci/run_healthy.sh
+++ experiments/nci/run_healthy.sh
@@ -14,8 +14,8 @@
 module load python3/3.10.4
 module load openmpi/5.0.5
 source /g/data/ny83/ch9972/NeuroStim/bin/activate
-cd /g/data/ny83/ch9972/NeuroStim/neurostimenv
+cd /g/data/ny83/ch9972/NeuroStim/neurostimenv/experiments/feature_analysis
 
-mpirun -np 512 python3 experiments/feature_analysis/run_simulations.py experiment.name=healthy experiment.seed=10 env=hl23net env.simulation.duration=100.0 env.network.dt=0.025 env.simulation.MDD=False env.ts.apply=False env.network.syn_activity=True experiment.debug=False
+mpirun -np 512 python3 run_simulations.py experiment.name=healthy experiment.seed=10 env=hl23net env.simulation.duration=28000.0 env.network.dt=0.025 env.simulation.MDD=False env.ts.apply=False env.network.syn_activity=True experiment.debug=False
 
 wait
